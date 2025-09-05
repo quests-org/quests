c26643708b11242ce76886d0050082d369ba1672
Jeremy Mack <mutewinter@users.noreply.github.com>
2025-09-05 10:44:35 -0500

    dx(workspace): defer tailwind cdn if internet is ever down
    because my internet went down

--- packages/workspace/src/logic/server/runtime-list.ts
+++ packages/workspace/src/logic/server/runtime-list.ts
@@ -42,7 +42,7 @@ export async function RuntimeList({
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://cdn.tailwindcss.com" defer></script>
         <title>Workspace Server</title>
         <script>
           let updateInterval;
