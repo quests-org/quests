9692176f2e7cb6eb8f0492a592a37dc150c140ce
Jeremy Mack <mutewinter@users.noreply.github.com>
2025-09-26 15:43:16 -0500

    refactor(workspace): make more semantic, remove duplicate parsing, use existing helpers

--- packages/workspace/src/lib/get-registry-apps.ts
+++ packages/workspace/src/lib/get-registry-apps.ts
@@ -1,18 +1,21 @@
 import fs from "node:fs/promises";
-import path from "node:path";
 import { z } from "zod";
 
 import { REGISTRY_APPS_FOLDER } from "../constants";
 import { AppDirSchema } from "../schemas/paths";
 import { SubdomainPartSchema } from "../schemas/subdomain-part";
-import { PREVIEW_SUBDOMAIN_PART } from "../schemas/subdomains";
+import {
+  PREVIEW_SUBDOMAIN_PART,
+  PreviewSubdomainSchema,
+} from "../schemas/subdomains";
 import { type WorkspaceConfig } from "../types";
 import { absolutePathJoin } from "./absolute-path-join";
+import { pathExists } from "./path-exists";
 
 export const RegistryAppSchema = z.object({
   appDir: AppDirSchema,
   folderName: z.string(),
-  previewSubdomain: z.string(),
+  previewSubdomain: PreviewSubdomainSchema,
 });
 
 type RegistryApp = z.output<typeof RegistryAppSchema>;
@@ -20,25 +23,21 @@ type RegistryApp = z.output<typeof RegistryAppSchema>;
 export async function getRegistryApps(
   workspaceConfig: WorkspaceConfig,
 ): Promise<RegistryApp[]> {
-  // First check if the templates dir exists
-  const appsDir = absolutePathJoin(
+  const registryAppsDir = absolutePathJoin(
     workspaceConfig.registryDir,
     REGISTRY_APPS_FOLDER,
   );
-  const appsDirExists = await fs
-    .stat(appsDir)
-    .then(() => true)
-    .catch(() => false);
-  if (!appsDirExists) {
+
+  if (!(await pathExists(registryAppsDir))) {
     return [];
   }
 
   try {
-    const entries = await fs.readdir(appsDir, { withFileTypes: true });
+    const entries = await fs.readdir(registryAppsDir, { withFileTypes: true });
     return entries
       .filter((entry) => entry.isDirectory() && !entry.name.startsWith("."))
       .map((entry) => {
-        const appDir = AppDirSchema.parse(path.resolve(appsDir, entry.name));
+        const appDir = absolutePathJoin(registryAppsDir, entry.name);
         return RegistryAppSchema.parse({
           appDir,
           folderName: entry.name,
@@ -47,7 +46,7 @@ export async function getRegistryApps(
       });
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.error("Error reading templates folder", error);
+    console.error("Error reading registry apps folder", error);
     return [];
   }
 }
