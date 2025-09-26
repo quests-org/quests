7cbb32a5b81d59689a840b846768dab663ff87c9
Jeremy Mack <mutewinter@users.noreply.github.com>
2025-09-26 11:14:24 -0500

    chore: proper glob for scripts folder

--- knip.config.ts
+++ knip.config.ts
@@ -6,7 +6,7 @@ const config: KnipConfig = {
     ".": {
       // Required because tailwindcss is loaded in root by Knip because of
       // eslint-plugin-better-tailwindcss
-      entry: ["scripts/*.{ts}"],
+      entry: ["scripts/*.ts"],
       ignoreDependencies: ["tailwindcss"],
     },
     "apps/api": {
