d0aec653a4b71d1ce6e79d8de64236bdc1257aca
Jeremy Mack <mutewinter@users.noreply.github.com>
2025-10-23 07:18:47 -0500

    feat(ai-gateway): provider-based defaults

--- packages/ai-gateway/src/lib/get-model-tags.ts
+++ packages/ai-gateway/src/lib/get-model-tags.ts
@@ -3,20 +3,18 @@ import { type AIGatewayProviderConfig } from "../schemas/provider-config";
 
 // cspell:ignore devstral
 const MODEL_TAGS: Record<string, AIGatewayModel.ModelTag[]> = {
-  "claude-3.7-sonnet": ["coding"],
-  "claude-haiku-4.5": ["coding", "recommended", "default"],
-  "claude-opus-4": ["coding"],
+  "claude-haiku-4.5": ["coding", "recommended", "default"], // Main default across all providers
   "claude-opus-4.1": ["coding"],
   "claude-sonnet-4": ["coding"],
   "claude-sonnet-4.5": ["coding", "recommended"],
   "devstral-medium-2507": ["coding", "recommended"],
   "gemini-2.5-flash": ["recommended"],
-  "gemini-2.5-pro": ["coding", "recommended", "default"],
+  "gemini-2.5-pro": ["coding", "recommended"],
   "glm-4.5": ["coding", "recommended"],
   "glm-4.5-air": ["coding", "recommended"],
   "glm-4.6": ["coding", "recommended"],
   "gpt-5": ["coding", "recommended"],
-  "gpt-5-codex": ["coding", "recommended", "default"],
+  "gpt-5-codex": ["coding", "recommended"],
   "gpt-5-mini": ["coding", "recommended"],
   "gpt-5-nano": ["recommended"],
   "grok-4": ["coding"],
@@ -29,6 +27,15 @@ const MODEL_TAGS: Record<string, AIGatewayModel.ModelTag[]> = {
   "qwen-3-coder-480b": ["coding", "recommended"], // Cerebras
 };
 
+// Models that we normally wouldn't set as default, but we for the author
+const DEFAULT_MODELS_BY_CONFIG_TYPE: Partial<
+  Record<AIGatewayProviderConfig.Type["type"], string[]>
+> = {
+  google: ["gemini-2.5-pro"],
+  openai: ["gpt-5-codex"],
+  "z-ai": ["glm-4.6"],
+};
+
 export function getModelTags(
   canonicalId: AIGatewayModel.CanonicalId,
   config: AIGatewayProviderConfig.Type,
@@ -38,6 +45,13 @@ export function getModelTags(
   // Don't recommend Codex for OpenAI compatible because it requires the responses API
   if (config.type === "openai-compatible" && canonicalId === "gpt-5-codex") {
     return tags.filter((tag) => tag !== "recommended" && tag !== "default");
+  }
+
+  const defaultModels = DEFAULT_MODELS_BY_CONFIG_TYPE[config.type] ?? [];
+  const isDefault = defaultModels.includes(canonicalId);
+
+  if (isDefault && !tags.includes("default")) {
+    return [...tags, "default"];
   }
 
   return [...tags];
