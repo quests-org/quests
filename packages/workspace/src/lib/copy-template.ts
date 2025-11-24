c97d5aa2e6c2f134fa805ca81d53056ff95fbfb8
Jeremy Mack <mutewinter@users.noreply.github.com>
2025-11-24 14:00:54 -0600

    fix(workspace): don't copy screenshots from templates

--- packages/workspace/src/lib/copy-template.ts
+++ packages/workspace/src/lib/copy-template.ts
@@ -14,33 +14,36 @@ export function copyTemplate({
   targetDir: AbsolutePath;
   templateDir: AbsolutePath;
 }) {
-  return (
-    ResultAsync.fromPromise(
-      getIgnore(templateDir),
-      (error) =>
-        new TypedError.FileSystem(
-          `Failed to get ignore patterns for template copy: ${error instanceof Error ? error.message : String(error)}`,
-          { cause: error },
-        ),
-    )
+  return ResultAsync.fromPromise(
+    getIgnore(templateDir),
+    (error) =>
+      new TypedError.FileSystem(
+        `Failed to get ignore patterns for template copy: ${error instanceof Error ? error.message : String(error)}`,
+        { cause: error },
+      ),
+  )
+    .andThen((ignore) => {
       // When starting from a template, don't use its existing manifest
-      .andThen((ignore) => ok(ignore.add(QUEST_MANIFEST_FILE_NAME)))
-      .andThen((ignore) =>
-        ResultAsync.fromPromise(
-          fs.cp(templateDir, targetDir, {
-            filter: (src) => {
-              const relativePath = path.relative(templateDir, src);
-              // Empty is root
-              return relativePath === "" || !ignore.ignores(relativePath);
-            },
-            recursive: true,
-          }),
-          (error) =>
-            new TypedError.FileSystem(
-              `Failed to copy template files: ${error instanceof Error ? error.message : String(error)}`,
-              { cause: error },
-            ),
-        ).map(() => true),
-      )
-  );
+      ignore.add(QUEST_MANIFEST_FILE_NAME);
+      // Ignore screenshots
+      ignore.add("screenshot.*");
+      return ok(ignore);
+    })
+    .andThen((ignore) =>
+      ResultAsync.fromPromise(
+        fs.cp(templateDir, targetDir, {
+          filter: (src) => {
+            const relativePath = path.relative(templateDir, src);
+            // Empty is root
+            return relativePath === "" || !ignore.ignores(relativePath);
+          },
+          recursive: true,
+        }),
+        (error) =>
+          new TypedError.FileSystem(
+            `Failed to copy template files: ${error instanceof Error ? error.message : String(error)}`,
+            { cause: error },
+          ),
+      ).map(() => true),
+    );
 }
