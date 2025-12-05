2c0b236ad360df9b9e55f2eefc3bd0ebd099a3d6
Jeremy Mack <mutewinter@users.noreply.github.com>
2025-12-05 08:26:06 -0600

    chore(studio): remove commented code

--- apps/studio/src/electron-main/api/constants.ts
+++ apps/studio/src/electron-main/api/constants.ts
@@ -4,7 +4,3 @@ export const API_AUTH_BASE_URL = import.meta.env.VITE_QUESTS_API_BASE_URL
 export const API_RPC_BASE_URL = import.meta.env.VITE_QUESTS_API_BASE_URL
   ? `${import.meta.env.VITE_QUESTS_API_BASE_URL}/rpc`
   : "";
-// export const API_GATEWAY_OPEN_ROUTER_BASE_URL = import.meta.env
-//   .VITE_QUESTS_API_BASE_URL
-//   ? `${import.meta.env.VITE_QUESTS_API_BASE_URL}/gateway/openrouter`
-//   : undefined;
