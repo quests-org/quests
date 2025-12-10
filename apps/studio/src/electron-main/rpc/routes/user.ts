ad65cf5f43515e6c68f9a66c556e28e571549751
Aaron Ortbals <aaron.ortbals@gmail.com>
2025-12-09 18:35:56 -0800

    lint(studio): zod import

--- apps/studio/src/electron-main/rpc/routes/user.ts
+++ apps/studio/src/electron-main/rpc/routes/user.ts
@@ -2,7 +2,7 @@ import { fetchSubscriptionStatus, getMe } from "@/electron-main/api/client";
 import { hasToken as hasTokenUtil } from "@/electron-main/api/utils";
 import { authenticatedOptional, base } from "@/electron-main/rpc/base";
 import { getProviderConfigsStore } from "@/electron-main/stores/provider-configs";
-import z from "zod";
+import { z } from "zod";
 
 const hasAIProviderConfig = base.handler(() => {
   const providersStore = getProviderConfigsStore();
