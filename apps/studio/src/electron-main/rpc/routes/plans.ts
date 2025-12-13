4b6f152c90ea6ff38aa5886896eebf0fefd921e2
Jeremy Mack <mutewinter@users.noreply.github.com>
2025-12-13 15:19:43 -0600

    chore(studio): use query client for plans

--- apps/studio/src/electron-main/rpc/routes/plans.ts
+++ apps/studio/src/electron-main/rpc/routes/plans.ts
@@ -1,8 +1,8 @@
-import { vanillaAPIRPCClient } from "@/electron-main/api/client";
+import { apiQueryClient, apiRPCClient } from "@/electron-main/api/client";
 import { base } from "@/electron-main/rpc/base";
 
 const get = base.handler(async () => {
-  return vanillaAPIRPCClient.plans.get();
+  return apiQueryClient.fetchQuery(apiRPCClient.plans.get.queryOptions());
 });
 
 export const plans = {
