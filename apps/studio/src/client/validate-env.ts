4fe3214a8b2101e53f6dfaf9ee95357cfd2355f3
Jeremy Mack <mutewinter@users.noreply.github.com>
2025-12-30 14:32:45 -0600

    chore(studio): remove unused env var

--- apps/studio/src/client/validate-env.ts
+++ apps/studio/src/client/validate-env.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   schema: {
     MAIN_VITE_GOOGLE_CLIENT_ID: z.string().optional(),
     MAIN_VITE_GOOGLE_CLIENT_SECRET: z.string().optional(),
-    MAIN_VITE_QUESTS_OPEN_ROUTER_BASE_URL: z.string().optional(),
     VITE_DEBUG_TELEMETRY: z.enum(["true", "false"]).default("false"),
     VITE_POSTHOG_API_HOST: z.string().optional(),
     VITE_POSTHOG_API_KEY: z.string().optional(),
